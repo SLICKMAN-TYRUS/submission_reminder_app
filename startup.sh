77861342b0e95b79e4aa22859a9de09b8b7fbe50
SLICKMAN-TYRUS <a.chol@alustudent.com>
2024-10-04 11:31:20 -0700

    Individualized Coding Project done!

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Step 1: Ensure we are in the correct directory
+cd submission_reminder_app
+
+# Step 2: Start the reminder app
+echo "Starting the Reminder App..."
+
+
+# Execute the reminder script
+bash ./app/reminder.sh
+
+
+echo " Reminder App has been executed successfully!"
